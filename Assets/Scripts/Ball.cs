e8fe1f27ff8e7bf04b797b8742804059274eec42
Eray Emir <128998801+peartolsamia@users.noreply.github.com>
2025-10-27 16:52:24 +0300

    Turned to vertical play and Ball.cs updated.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,19 +1,43 @@
 using UnityEngine;
 
+
 public class Ball : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
-        GetComponent<Rigidbody2D>().linearVelocity = Vector3.up * moveSpeed;
+        GetComponent<Rigidbody2D>().linearVelocity = Vector2.up * moveSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    float hitFactor(Vector2 ballPos, Vector2 platePos, float plateHeight)
     {
-        
+        return (ballPos.x - platePos.x) / plateHeight;
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Plate(Down)")
+        {
+            float x = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+
+            Vector2 direction = new Vector2(x, -1).normalized;
+
+            GetComponent<Rigidbody2D>().linearVelocity = direction * moveSpeed;
+        }
+
+
+        if (collision.gameObject.name == "Plate(Up)")
+        {
+            float x = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+
+            Vector2 direction = new Vector2(x, -1).normalized;
+
+            GetComponent<Rigidbody2D>().linearVelocity = direction * moveSpeed;
+        }
     }
 }
